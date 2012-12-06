e5f0b5e0e9e76d48c8e7c6df5fb799ab3d69e5e9
cynipe <cynipe@gmail.com>
2012-12-06 20:13:31 +0900

    Remove ubuntu support for a while,  I'll be back soon!

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,58 +21,59 @@ class icinga::params {
 
   case $::operatingsystem {
     'Debian', 'Ubuntu': {
-      $package = $::lsbdistcodename ? {
-        'squeeze' => 'pnp4nagios/squeeze-backports',
-        default   => 'pnp4nagios',
-      }
-
-      $package_client_ensure     = 'present'
-      $package_server_ensure     = 'present'
-      $package_client            = [ 'nagios-nrpe-server', 'nagios-plugins' ]
-      $package_server            = [ 'icinga', 'icinga-core', 'icinga-common', 'icinga-cgi', 'icinga-phpapi', 'nagios-nrpe-plugin' ]
-      $service_client            = 'nagios-nrpe-server'
-      $service_client_ensure     = 'running'
-      $service_client_enable     = true
-      $service_client_hasstatus  = false
-      $service_client_hasrestart = true
-      $service_client_pattern    = 'nrpe'
-      $service_server            = 'icinga'
-      $service_server_ensure     = 'running'
-      $service_server_enable     = true
-      $service_server_hasstatus  = true
-      $service_server_hasrestart = true
-      $pidfile_client            = '/var/run/nagios/nrpe.pid'
-      $pidfile_server            = '/var/run/icinga/icinga.pid'
-      $confdir_client            = '/etc/nagios'
-      $confdir_server            = '/etc/icinga'
-      $vardir_client             = '/var/lib/nagios'
-      $vardir_server             = '/var/lib/icinga'
-      $sharedir_server           = '/usr/share/icinga/htdocs'
-      $includedir_client         = '/etc/nagios/nrpe.d'
-      $usrlib                    = '/usr/lib'
-      $plugindir                 = "${usrlib}/nagios/plugins"
-      $service_webserver         = 'apache2'
-      $webserver_user            = 'www-data'
-      $webserver_group           = 'www-data'
-      $server_user               = 'nagios'
-      $server_group              = 'nagios'
-      $client_user               = $server_user
-      $client_group              = $server_group
-      $server_cmd_group          = $server_group
-      $htpasswd_file             = "${confdir_server}/htpasswd.users"
-      $targetdir                 = "${confdir_server}/objects"
-      $targetdir_contacts        = "${targetdir}/contacts/contacts.cfg"
-      $icinga_vhost              = '/etc/icinga/apache2.conf'
-      $logdir_client             = '/var/log/nrpe'
-      $logdir_server             = '/var/log/icinga'
-
-      # IDOUTILS: TO BE REFACTORED
-      $idoutils_pkg     = 'icinga-idoutils'
-      $idoutils_confdir = '/etc/icinga/idoutils'
-      $idoutils_service = 'ido2db'
-      $idoutils_dbname  = 'icinga'
-      $idoutils_dbuser  = 'icinga'
-      $idoutils_dbpass  = 'icinga'
+      fail 'Currently not supported.'
+      #$package = $::lsbdistcodename ? {
+      #  'squeeze' => 'pnp4nagios/squeeze-backports',
+      #  default   => 'pnp4nagios',
+      #}
+      #
+      #$package_client_ensure     = 'present'
+      #$package_server_ensure     = 'present'
+      #$package_client            = [ 'nagios-nrpe-server', 'nagios-plugins' ]
+      #$package_server            = [ 'icinga', 'icinga-api', 'icinga-doc', 'icinga-gui', 'nagios-plugins-nrpe' ]
+      #$service_client            = 'nagios-nrpe-server'
+      #$service_client_ensure     = 'running'
+      #$service_client_enable     = true
+      #$service_client_hasstatus  = false
+      #$service_client_hasrestart = true
+      #$service_client_pattern    = 'nrpe'
+      #$service_server            = 'icinga'
+      #$service_server_ensure     = 'running'
+      #$service_server_enable     = true
+      #$service_server_hasstatus  = true
+      #$service_server_hasrestart = true
+      #$pidfile_client            = '/var/run/nagios/nrpe.pid'
+      #$pidfile_server            = '/var/run/icinga/icinga.pid'
+      #$confdir_client            = '/etc/nagios'
+      #$confdir_server            = '/etc/icinga'
+      #$vardir_client             = '/var/lib/nagios'
+      #$vardir_server             = '/var/lib/icinga'
+      #$sharedir_server           = '/usr/share/icinga/htdocs'
+      #$includedir_client         = '/etc/nagios/nrpe.d'
+      #$usrlib                    = '/usr/lib'
+      #$plugindir                 = "${usrlib}/nagios/plugins"
+      #$service_webserver         = 'apache2'
+      #$webserver_user            = 'www-data'
+      #$webserver_group           = 'www-data'
+      #$server_user               = 'nagios'
+      #$server_group              = 'nagios'
+      #$client_user               = $server_user
+      #$client_group              = $server_group
+      #$server_cmd_group          = $server_group
+      #$htpasswd_file             = "${confdir_server}/htpasswd.users"
+      #$targetdir                 = "${confdir_server}/objects"
+      #$targetdir_contacts        = "${targetdir}/contacts/contacts.cfg"
+      #$icinga_vhost              = '/etc/icinga/apache2.conf'
+      #$logdir_client             = '/var/log/nrpe'
+      #$logdir_server             = '/var/log/icinga'
+      #
+      ## IDOUTILS: TO BE REFACTORED
+      #$idoutils_pkg     = 'icinga-idoutils'
+      #$idoutils_confdir = '/etc/icinga/idoutils'
+      #$idoutils_service = 'ido2db'
+      #$idoutils_dbname  = 'icinga'
+      #$idoutils_dbuser  = 'icinga'
+      #$idoutils_dbpass  = 'icinga'
     }
 
     'RedHat', 'CentOS', 'Scientific', 'OEL', 'Amazon': {
